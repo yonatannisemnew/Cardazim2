17b6be63f46e48c4f6ecf974aea71a6da4212f58
yonatannisemnew <yonatan.nissenbaum1@mail.huji.ac.il>
2024-09-21 21:52:31 +0300

    Update server.py

--- client_server/server.py
+++ client_server/server.py
@@ -3,36 +3,28 @@ import time
 import socket
 import struct
 import threading
-from _thread import *
+from listener import  Listener
 
-
-import sys
 
 def connect_to_client(conn, addrs):
     en_leangth = conn.recv(4)
     leangth = struct.unpack('<i', en_leangth)
     en_messege = conn.recv(leangth[0])
     messege = en_messege.decode()
-    print(f" {messege} from ip {addrs} and thread {os.getpid()}")
+    print(f" {messege} from ip {addrs} ")
+    time.sleep(5)
     conn.close()
+    print(f"connection from {threading.get_ident()} is closed")
 
 def run_server(port, ip):
-    server_socket = socket.socket()
-    server_socket.bind((ip, port)) # bind host address and port together
-    server_socket.listen()
-    try:
+    with Listener("127.0.0.1", 8765) as listener:
         while True:
-            conn, addrs = server_socket.accept()
-            client_thread = threading.Thread(target=connect_to_client, args=(conn, addrs))
-            client_thread.start()
-
-
-    except KeyboardInterrupt:
-        server_socket.close()
-
+            with listener.accept() as conn:
+                client_thread = threading.Thread(target=print(conn.receive_message()), args=())
+                client_thread.start()
 
 def main():
-    run_server(8888, "0.0.0.0")
+    run_server(8765, "0.0.0.0")
 
 
 if __name__ == '__main__':
