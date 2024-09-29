a8736c6d9af73a0c59776bdbca7a7239aa43dcef
yonatannisemnew <yonatan.nissenbaum1@mail.huji.ac.il>
2024-09-29 13:03:21 +0300

    Update server.py

--- client_server/server.py
+++ client_server/server.py
@@ -1,15 +1,24 @@
 
 import threading
 from listener import Listener
+from card import Card
+from CardManager import CardManager
+
+
+def save_to_database(card: Card):
+    print("Received card.")
+    manager = CardManager('./database')
+    manager.save(card)
+    print(f"Saved card to path ./database/{card.creator}/{card.name}")
 def run_server(port, ip):
-    with Listener("127.0.0.1", 8765) as listener:
+    with Listener("127.0.0.1", 8745) as listener:
         while True:
             with listener.accept() as conn:
-                client_thread = threading.Thread(target=print(conn.receive_message()), args=())
+                client_thread = threading.Thread(target=save_to_database(conn.receive_message()), args=())
                 client_thread.start()
 
 def main():
-    run_server(8765, "0.0.0.0")
+    run_server(8745, "0.0.0.0")
 
 
 if __name__ == '__main__':
